b74d9ac30ff79cb77dc2735ea3fc79b61b9e70ef
subash.kharel.123@outlook.com <librarycheckoutproj@gmail.com>
2023-11-07 09:28:05 -0600

    working state for Highly available eureka server

--- parent/catalog/src/main/java/com/product/catalog/CatalogApplication.java
+++ parent/catalog/src/main/java/com/product/catalog/CatalogApplication.java
@@ -2,9 +2,11 @@ package com.product.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 //@EnableFeignClients
+@EnableDiscoveryClient
 public class CatalogApplication {
 
 	public static void main(String[] args) {
